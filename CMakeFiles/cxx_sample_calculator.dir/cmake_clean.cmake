84e17371a9bd73c733c1a0b28de18ae248245481
Anh Phan <17020589@vnu.edu.vn>
2020-09-04 11:02:57 +0700

    .

---
+++ CMakeFiles/cxx_sample_calculator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cxx_sample_calculator.dir/Calculator.cpp.o"
+  "CMakeFiles/cxx_sample_calculator.dir/Menu.cpp.o"
+  "CMakeFiles/cxx_sample_calculator.dir/main.cpp.o"
+  "cxx_sample_calculator"
+  "cxx_sample_calculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cxx_sample_calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
