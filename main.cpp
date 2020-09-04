645b4559fb1d31e961b53523d5cd639f48ea5233
Anh Phan <17020589@vnu.edu.vn>
2020-09-04 10:36:10 +0700

    Revert "Merge branch 'finstrument-functions'"
    This reverts commit 94992de6935d077411d9589037067c8bb19f42da.

--- main.cpp
+++ main.cpp
@@ -4,31 +4,25 @@
 #include <cstdio>
 #include "Menu.h"
 
-extern "C" {
-    void __cyg_profile_func_enter (void *this_fn, void *call_site) __attribute__((no_instrument_function));
-    void __cyg_profile_func_exit (void *this_fn, void *call_site) __attribute__((no_instrument_function));
-
-    void __cyg_profile_func_enter (void *this_fn, void *call_site)
-    {
-        Dl_info info;
-        dladdr(this_fn, &info);
-    //    char *outputBuffer = NULL;
-    //    char* realname = abi::__cxa_demangle(info.dli_sname, 0, 0, 0);
-        printf("%p\t", this_fn);
-        printf("Function Entry : %s %s\n", info.dli_fname, info.dli_sname);
-    }
-
-    void __cyg_profile_func_exit (void *this_fn, void *call_site)
-    {
-        Dl_info info;
-        dladdr(this_fn, &info);
-    //    char* outputBuffer = NULL;
-    //    char* realname = abi::__cxa_demangle(info.dli_sname, 0, 0, 0);
-        printf("%p\t", this_fn);
-        printf("Function Exit : %s %s\n", info.dli_fname, info.dli_sname);
-    }
+static void __attribute__((no_instrument_function)) __cyg_profile_func_enter (void *this_fn, void *call_site)
+{
+    Dl_info info;
+    dladdr(this_fn, &info);
+//    char *outputBuffer = NULL;
+//    char* realname = abi::__cxa_demangle(info.dli_sname, 0, 0, 0);
+    printf("%p\t", this_fn);
+    printf("Function Entry : %s %s\n", info.dli_fname, info.dli_sname);
 }
 
+static void __attribute__((no_instrument_function)) __cyg_profile_func_exit (void *this_fn, void *call_site)
+{
+    Dl_info info;
+    dladdr(this_fn, &info);
+//    char* outputBuffer = NULL;
+//    char* realname = abi::__cxa_demangle(info.dli_sname, 0, 0, 0);
+    printf("%p\t", this_fn);
+    printf("Function Exit : %s %s\n", info.dli_fname, info.dli_sname);
+}
 
 int foo() {
     std::cout << "Inside foo" << std::endl;
@@ -42,8 +36,8 @@ int foo2(int param) {
 
 int main() {
     Menu* menu = new Menu();
-    menu->init();
-//    int x = foo();
-//    foo2(x);
+//    menu->init();
+    int x = foo();
+    foo2(x);
     return 0;
 }
